9c55a2964d850da1c92e022c705d19e217a79b80
Brandon Chong <brandonchong9@gmail.com>
2013-05-20 14:26:16 -0700

    Brandon's First Commit

--- HW Schedule/src/general/Controller.java
+++ HW Schedule/src/general/Controller.java
@@ -9,7 +9,7 @@ import view.*;
 
 public class Controller implements ScheduleViewDelegate, ScheduleViewDataSource {
 	
-	private Curriculum currentCurriculum;
+	private Curriculum currentCurriculum; //Brandon's first commit
 	
 	public static void main(String[] args) {
 		new Controller();
